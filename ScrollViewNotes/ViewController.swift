d9a26de40d4a1e113888669392924bc4b2117508
Max Widhalm <max.widhalm23@d155.org>
2023-02-17 07:56:47 -0600

    Initial Commit

---
+++ ScrollViewNotes/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  ScrollViewNotes
+//
+//  Created by MAX WIDHALM on 2/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        scrollView.delegate = self
+        scrollView.backgroundColor = UIColor.blue
+        
+        
+    }
+    
+    @IBAction func vcAction(_ sender: UIButton) {
+        performSegue(withIdentifier: "segue", sender: nil)
+    }
+    
+    @IBAction func bracketAction(_ sender: UIButton) {
+    }
+    
+    
+
+}
+
